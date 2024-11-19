6e2ec18bd5485ee46a345007bbf8becd6409da01
zawmyo <jumpzaw14928@gmail.com>
2024-11-19 11:52:25 +0630

    create models comment

--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from employees.models import Employee
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    employess =Employee.objects.all()
+    context = {
+        'employess':employess
+    }
+    return render(request, 'pages/index.html',context)
